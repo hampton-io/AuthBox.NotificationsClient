2dd51a41b1b727a5cd43b33db0845e359ce19399
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-06-27 22:54:49 +0100

    Change first name last name to just be name for access request email

--- test/sendAccessRequest.test.js
+++ test/sendAccessRequest.test.js
@@ -5,8 +5,7 @@ describe('when sending an access request email', () => {
 
   const connectionString = 'some-redis-connection';
   const email = 'user.one@unit.test';
-  const firstName = 'Test';
-  const lastName = 'Tester';
+  const name = 'Test Tester';
   const orgName = 'My Org';
   const approved = true;
   const reason = 'reason';
@@ -44,63 +43,57 @@ describe('when sending an access request email', () => {
   });
 
   test('then it should create queue connecting to provided connection string', async () => {
-    await client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason);
+    await client.sendAccessRequest(email, name, orgName, approved, reason);
 
     expect(createQueue.mock.calls.length).toBe(1);
     expect(createQueue.mock.calls[0][0].redis).toBe(connectionString);
   });
 
   test('then it should create job with type of accessrequest_v1', async () => {
-    await client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason);
+    await client.sendAccessRequest(email, name, orgName, approved, reason);
 
     expect(create.mock.calls.length).toBe(1);
     expect(create.mock.calls[0][0]).toBe('accessrequest_v1');
   });
 
   test('then it should create job with data including email', async () => {
-    await client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason);
+    await client.sendAccessRequest(email, name, orgName, approved, reason);
 
     expect(create.mock.calls[0][1].email).toBe(email);
   });
 
-  test('then it should create job with data including first name', async () => {
-    await client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason);
+  test('then it should create job with data including name', async () => {
+    await client.sendAccessRequest(email, name, orgName, approved, reason);
 
-    expect(create.mock.calls[0][1].firstName).toBe(firstName);
-  });
-
-  test('then it should create job with data including last name', async () => {
-    await client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason);
-
-    expect(create.mock.calls[0][1].lastName).toBe(lastName);
+    expect(create.mock.calls[0][1].name).toBe(name);
   });
 
   test('then it should create job with data including org name', async () => {
-    await client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason);
+    await client.sendAccessRequest(email, name, orgName, approved, reason);
 
     expect(create.mock.calls[0][1].orgName).toBe(orgName);
   });
 
   test('then it should create job with data including approved', async () => {
-    await client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason);
+    await client.sendAccessRequest(email, name, orgName, approved, reason);
 
     expect(create.mock.calls[0][1].approved).toBe(approved);
   });
 
   test('then it should create job with data including reason', async () => {
-    await client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason);
+    await client.sendAccessRequest(email, name, orgName, approved, reason);
 
     expect(create.mock.calls[0][1].reason).toBe(reason);
   });
 
   test('then it should save the job', async () => {
-    await client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason);
+    await client.sendAccessRequest(email, name, orgName, approved, reason);
 
     expect(jobSave.mock.calls.length).toBe(1);
   });
 
   test('then it should resolve if there is no error', async () => {
-    await expect(client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason)).resolves.toBeUndefined();
+    await expect(client.sendAccessRequest(email, name, orgName, approved, reason)).resolves.toBeUndefined();
   });
 
   test('then it should reject if there is an error', async () => {
@@ -108,7 +101,7 @@ describe('when sending an access request email', () => {
       callback('Unit test error');
     };
 
-    await expect(client.sendAccessRequest(email, firstName, lastName, orgName, approved, reason)).rejects.toBeDefined();
+    await expect(client.sendAccessRequest(email, name, orgName, approved, reason)).rejects.toBeDefined();
   });
 
 });
