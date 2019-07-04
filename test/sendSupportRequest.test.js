6155dccec6157fde46e56589f10fe7c6d1be501c
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-07-04 16:26:15 +0100

    NSA-2311 - remove unrequired reference

--- test/sendSupportRequest.test.js
+++ test/sendSupportRequest.test.js
@@ -10,7 +10,6 @@ describe('when sending a support request', () => {
   const service = 'DfE Sign-in Client Service';
   const type = 'I have multiple accounts';
   const message = 'I am having trouble signing in using my new details';
-  const reference = 'SIN123456798';
   const orgName = 'org1';
   const urn = '123345';
 
@@ -47,21 +46,21 @@ describe('when sending a support request', () => {
   });
 
   test('then it should create queue connecting to provided connection string', async () => {
-    await client.sendSupportRequest(name, email, phone, service, type, message, reference, orgName, urn);
+    await client.sendSupportRequest(name, email, phone, service, type, message, orgName, urn);
 
     expect(createQueue.mock.calls.length).toBe(1);
     expect(createQueue.mock.calls[0][0].redis).toBe(connectionString);
   });
 
   test('then it should create job with type of supportrequest_v1', async () => {
-    await client.sendSupportRequest(name, email, phone, service, type, message, reference, orgName, urn);
+    await client.sendSupportRequest(name, email, phone, service, type, message, orgName, urn);
 
     expect(create.mock.calls.length).toBe(1);
     expect(create.mock.calls[0][0]).toBe('supportrequest_v1');
   });
 
   test('then it should create job with data in call', async () => {
-    await client.sendSupportRequest(name, email, phone, service, type, message, reference, orgName, urn);
+    await client.sendSupportRequest(name, email, phone, service, type, message, orgName, urn);
 
     expect(create.mock.calls[0][1]).toEqual({
       name,
@@ -70,20 +69,19 @@ describe('when sending a support request', () => {
       service,
       type,
       message,
-      reference,
       orgName,
       urn,
     });
   });
 
   test('then it should save the job', async () => {
-    await client.sendSupportRequest(name, email, phone, service, type, message, reference, orgName, urn);
+    await client.sendSupportRequest(name, email, phone, service, type, message, orgName, urn);
 
     expect(jobSave.mock.calls.length).toBe(1);
   });
 
   test('then it should resolve if there is no error', async () => {
-    await expect(client.sendSupportRequest(name, email, phone, service, type, message, reference, orgName, urn)).resolves.toBeUndefined();
+    await expect(client.sendSupportRequest(name, email, phone, service, type, message, orgName, urn)).resolves.toBeUndefined();
   });
 
   test('then it should reject if there is an error', async () => {
@@ -91,7 +89,7 @@ describe('when sending a support request', () => {
       callback('Unit test error');
     };
 
-    await expect(client.sendSupportRequest(name, email, phone, service, type, message, reference, orgName, urn)).rejects.toBeDefined();
+    await expect(client.sendSupportRequest(name, email, phone, service, type, message, orgName, urn)).rejects.toBeDefined();
   });
 
 });
